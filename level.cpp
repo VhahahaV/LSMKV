36163e1abab79cf058ec5e613679377a7682dd0a
VhahahaV <czq666@sjtu.edu.cn>
2024-05-28 17:10:30 +0800

    add level

---
+++ level.cpp
@@ -0,0 +1,5 @@
+//
+// Created by VhahahaV on 2024/5/28.
+//
+
+#include "level.h"
---
+++ level.h
@@ -0,0 +1,17 @@
+//
+// Created by VhahahaV on 2024/5/28.
+//
+
+#ifndef KVLSM_LEVEL_H
+#define KVLSM_LEVEL_H
+
+#include "ssTable.h"
+#include <vector>
+class level {
+    std::vector<SSTable> ssTableCache;
+
+
+};
+
+
+#endif //KVLSM_LEVEL_H
